52f71a1ec66460ac4cc3c3aff423998b31491c29
MaxVoicu7 <maxim.voicu@iis.utm.md>
2023-11-28 12:54:13 +0200

    working on the connection to the database

--- config/config.py
+++ config/config.py
@@ -3,5 +3,12 @@ import os
 
 load_dotenv()  # Încarcă variabilele de mediu din .env
 
+# bot constants
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 BOTNAME = os.getenv('BOTNAME')
+
+# database connection constants
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
